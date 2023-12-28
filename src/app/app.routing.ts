3c35ac3a9456305d0421fe5ea91892650877b6cc
Steffen Hiemer <steffenhiemer@web.de>
2023-12-28 17:43:55 +0100

    Setup Routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,18 @@ import {NumberComponent} from "./numberInfos/number/number.component";
 import {GameComponent} from "./game/game.component";
 import {UserProfileComponent} from "./user/user-profile/user-profile.component";
 import {LoginComponent} from "./pages/login/login.component";
+import {ContactComponent} from "./pages/contact/contact.component";
+import {ImpressumComponent} from "./pages/impressum/impressum.component";
+import {DatenschutzComponent} from "./pages/datenschutz/datenschutz.component";
+import {AgbComponent} from "./pages/agb/agb.component";
+import {BackButtonWithFooterComponent} from "./layout/back-button-with-footer/back-button-with-footer.component";
+
+const LayoutSimple: Routes = [
+  { path: 'impressum', component: ImpressumComponent},
+  { path: 'kontakt', component: ContactComponent},
+  { path: 'datenschutz', component: DatenschutzComponent },
+  { path: 'agb', component: AgbComponent },
+];
 
 const APP_ROUTES: Routes = [
   { path: '', component: HomeComponent},
@@ -12,7 +24,12 @@ const APP_ROUTES: Routes = [
   { path: 'numbers', component: NumbersComponent},
   { path: 'number/:number', component: NumberComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'active-game', component: GameComponent}
+
+  { path: 'active-game', component: GameComponent},
+
+  // FOOTER
+  { path: '', component: BackButtonWithFooterComponent, children: LayoutSimple },
 ]
+
 
 export const routing = RouterModule.forRoot(APP_ROUTES)
